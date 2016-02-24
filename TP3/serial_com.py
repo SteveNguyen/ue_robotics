a860a0ff5f1be57895b56a8a38b4b8b84d48fc0c
Remi Fabre <remifabre1800@gmail.com>
2016-02-24 20:00:33 +0100

    Adding TP5

--- TP3/serial_com.py
+++ TP3/serial_com.py
@@ -4,8 +4,8 @@
 import serial
 
 
-def open_serial(port, baud):
-    ser = serial.Serial(port=port, baudrate=baud)
+def open_serial(port, baud, timeout):
+    ser = serial.Serial(port=port, baudrate=baud, timeout=timeout)
     if ser.isOpen():
         return ser
     else:
@@ -35,17 +35,20 @@ def decode_data(data):
 
     return res
 
+def checksum(data) :
+    return (~data)&0xff
+
 if __name__ == '__main__':
 
     # we open the port
-    serial_port = open_serial('/dev/ttyUSB0', 1000000, timeout=0.1)
+    serial_port = open_serial('/dev/ttyS0', 1000000, timeout=0.1)
 
     # we create the packet for a LED ON command
     # two start bytes
     data_start = to_hex(0xff)
 
     # id of the motor (here 1), you need to change
-    data_id = to_hex(0x01)
+    data_id = to_hex(0x02)
 
     # lenght of the packet
     data_lenght = to_hex(0x04)
@@ -58,11 +61,13 @@ if __name__ == '__main__':
     data_param2 = to_hex(0x01)  # write 0x01
 
     # checksum (read the doc)
-    data_checksum = to_hex(0xdd)
+    data_checksum = checksum(ord(data_id) + ord(data_lenght) + ord(data_instruction) + ord(data_param1) + ord(data_param2))#to_hex(0xdd)
 
+    print "checksum = ", data_checksum
+    
     # we concatenate everything
     data = data_start + data_start + data_id + data_lenght + \
-        data_instruction + data_param1 + data_param2 + data_checksum
+        data_instruction + data_param1 + data_param2 + chr(data_checksum)
 
     print decode_data(data)
     write_data(serial_port, data)
